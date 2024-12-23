3dab7e4aa70233e2dd6a6a28f94d2b38dbab85ab
averyreyna <avery.reyna16@gmail.com>
2024-12-23 15:51:09 -0500

    started skeleton of migrating current website

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,14 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Hello! I'm Avery Reyna.
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        I'm a Software Engineer working on data quality management at Capital One. Before that, I researched human-computer interaction and computational social science with supervision from Pamela Wisniewski and Jonathan Powell. I also dove into collaborations with teams improving developer experience at ActBlue, building predictive models at Swing Left, and researching technology policy at New America.
       </p>
-      <div className="my-8">
+      {/* <div className="my-8">
         <BlogPosts />
-      </div>
+      </div> */}
     </section>
   )
 }
