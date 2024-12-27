08ae914d481eaa585f484786cf1f7405faaea9e2
averyreyna <avery.reyna16@gmail.com>
2024-12-26 19:26:54 -0500

    fixed errors with projects page and updated some styling

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -1,33 +1,42 @@
 import Link from 'next/link'
-import { formatDate, getBlogPosts } from 'app/blog/utils'
 
-export function BlogPosts() {
-  let allBlogs = getBlogPosts()
+interface Project {
+  title: string
+  link: string
+  date: string
+}
 
+const projects: Project[] = [
+  {
+    title: 'Help Supporters',
+    link: 'https://dl.acm.org/doi/10.1145/3613904.3642816',
+    date: '2024-03-01'
+  }
+]
+
+export function Projects() {
   return (
     <div>
-      {allBlogs
+      {projects
         .sort((a, b) => {
-          if (
-            new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
-          ) {
+          if (new Date(a.date) > new Date(b.date)) {
             return -1
           }
           return 1
         })
-        .map((post) => (
+        .map((project) => (
           <Link
-            key={post.slug}
+            key={project.title}
             className="flex flex-col space-y-1 mb-4"
-            href={`/blog/${post.slug}`}
+            href={project.link}
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
               <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
-                {formatDate(post.metadata.publishedAt)}
+                {new Date(project.date).getFullYear()}
               </p>
               <div>
                 <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                  {post.metadata.title}
+                  {project.title}
                 </p>
               </div>
             </div>
