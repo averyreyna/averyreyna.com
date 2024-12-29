45a2ac9271995ff8ce90f401409d58e08abbea65
averyreyna <avery.reyna16@gmail.com>
2024-12-29 00:53:35 -0500

    fixed year styling and other css changes

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -3,6 +3,7 @@ import { formatDate, getProjectPosts } from 'app/projects/utils'
 
 export function ProjectPosts() {
   let allProjects = getProjectPosts()
+  const usedYears = new Set<number>()
 
   return (
     <div>
@@ -23,7 +24,7 @@ export function ProjectPosts() {
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
               <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
-                {formatDate(post.metadata.publishedAt)}
+                {formatDate(post.metadata.publishedAt, usedYears)}
               </p>
               <div>
                 <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
