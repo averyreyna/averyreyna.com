de5d83e7b4599d70292b2fc01f6effd5478e9f41
averyreyna <avery.reyna16@gmail.com>
2025-01-03 18:44:49 -0500

    started editing home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Footer from "./components/footer"
 import Publications from "./components/publications"
 import Connections from "./components/connections"
+import Link from 'next/link'
+import { ArrowIcon } from "./components/footer"
 
 export default function Page() {
   return (
@@ -12,23 +14,29 @@ export default function Page() {
         I'm a Software Engineer working on data quality at Capital One. Before that, I researched computational social science and human-computer interaction with supervision from Jonathan Powell and Pamela Wisniewski. I also collaborated with teams improving developer experience at ActBlue, building predictive models at Swing Left, and researching technology policy at New America.
       </p>
 
-      <h2 className="mb-4 text-2xl font-semibold tracking-tighter">
-        Recent Publications
-      </h2>
+      <Link href="/publications" className="group inline-flex items-center">
+        <h2 className="mb-4 text-2xl font-semibold tracking-tighter transition-all relative active:opacity-70 sm:active:opacity-100">
+          Publications
+          <span className="hidden sm:items-center text-neutral-600 dark:text-neutral-400 hover:text-neutral-800 dark:hover:text-neutral-100 transition-all opacity-0 translate-x-[-8px] sm:group-hover:opacity-100 sm:group-hover:translate-x-0 duration-200 ml-4 text-xs sm:text-base whitespace-nowrap inline-flex">
+            <ArrowIcon />
+            <span className="ml-2 flex items-center">All</span>
+          </span>
+        </h2>
+      </Link>
       <div className="mb-4">
         <Publications />
       </div>
 
       <h2 className="mb-4 text-2xl font-semibold tracking-tighter">
-        Concept Mesh
+        Connections
       </h2>
       <div className="mb-4">
         <Connections />
       </div>
 
-    <h2 className="mb-4 text-2xl font-semibold tracking-tighter">
-      Contact
-    </h2>
+      <h2 className="mb-4 text-2xl font-semibold tracking-tighter">
+        Contact
+      </h2>
 
       <Footer />
     </section>
