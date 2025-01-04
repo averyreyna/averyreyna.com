ae96375e0dcee1fa2739c6e0703874f48ccd5b37
averyreyna <avery.reyna16@gmail.com>
2025-01-03 19:08:02 -0500

    more link styling changes

--- app/page.tsx
+++ app/page.tsx
@@ -14,10 +14,16 @@ export default function Page() {
         I'm a Software Engineer working on data quality at Capital One. Before that, I researched computational social science and human-computer interaction with supervision from Jonathan Powell and Pamela Wisniewski. I also collaborated with teams improving developer experience at ActBlue, building predictive models at Swing Left, and researching technology policy at New America.
       </p>
 
-      <Link href="/publications" className="group inline-flex items-center">
+      <div className="block sm:hidden">
+        <h2 className="mb-4 text-2xl font-semibold tracking-tighter">
+          Publications
+        </h2>
+      </div>
+
+      <Link href="/publications" className="hidden sm:inline-flex group items-center">
         <h2 className="mb-4 text-2xl font-semibold tracking-tighter transition-all relative active:opacity-70 sm:active:opacity-100">
           Publications
-          <span className="hidden sm:items-center text-neutral-600 dark:text-neutral-400 hover:text-neutral-800 dark:hover:text-neutral-100 transition-all opacity-0 translate-x-[-8px] sm:group-hover:opacity-100 sm:group-hover:translate-x-0 duration-200 ml-4 text-xs sm:text-base whitespace-nowrap inline-flex">
+          <span className="items-center text-neutral-600 dark:text-neutral-400 hover:text-neutral-800 dark:hover:text-neutral-100 transition-all opacity-0 translate-x-[-8px] group-hover:opacity-100 group-hover:translate-x-0 duration-200 ml-4 text-xs sm:text-base whitespace-nowrap inline-flex">
             <ArrowIcon />
             <span className="ml-2 flex items-center">All</span>
           </span>
