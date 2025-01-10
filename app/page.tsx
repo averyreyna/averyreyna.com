7c84e1455397f0dad197131da765ce860c07bd05
averyreyna <avery.reyna16@gmail.com>
2025-01-09 21:59:00 -0500

    updated font to abc oracle

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
         Hello! I'm Avery Reyna.
       </h1>
       <p className="mb-4">
-        I'm a Software Engineer working on data quality at Capital One. Before that, I researched computational social science and human-computer interaction with supervision from Jonathan Powell and Pamela Wisniewski. I also collaborated with teams improving developer experience at ActBlue, building predictive models at Swing Left, and researching technology policy at New America.
+        I'm a Software Engineer working on data quality at Capital One. Before that, I researched computational social science and human-computer interaction with supervision from Jonathan Powell and Pamela Wisniewski. I also worked with teams improving developer experience at ActBlue, building predictive models at Swing Left, and researching technology policy at New America.
       </p>
 
       <h2 className="mb-4 text-2xl font-semibold tracking-tighter">
