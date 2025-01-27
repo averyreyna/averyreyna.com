bb9c07995238dbeb5085759979f08ed0c90a7d81
averyreyna <avery.reyna16@gmail.com>
2025-01-26 22:36:01 -0500

    finsihed updating linked, adjusted mobile styling

--- app/components/scholarship.tsx
+++ app/components/scholarship.tsx
@@ -117,11 +117,11 @@ export default function Scholarship() {
               key={item.title}
               className="flex flex-col space-y-1 mb-4"
             >
-              <div className="w-full flex flex-row items-start">
-                <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
+              <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
+                <p className="text-neutral-600 dark:text-neutral-400 w-full md:w-[100px] tabular-nums">
                   {showYear ? item.year : ''}
                 </p>
-                <div className="flex-1 pl-2">
+                <div className="flex-1">
                   <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
                     {item.title}
                   </p>
